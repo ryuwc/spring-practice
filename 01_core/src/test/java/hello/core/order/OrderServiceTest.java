3bd6008284ddb23ed63fd6ff24cceb83b2434e58
ryuwc <wom05169@gmail.com>
2023-02-22 22:34:37 +0900

    스프링 1일차 마무리

--- 01_core/src/test/java/hello/core/order/OrderServiceTest.java
+++ 01_core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,16 +1,20 @@
 package hello.core.order;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    AppConfig appConfig = new AppConfig();
+    MemberService memberService = appConfig.memberService();
+    OrderService orderService = appConfig.orderService();
+
+//    MemberService memberService = new MemberServiceImpl();
+//    OrderService orderService = new OrderServiceImpl();
 
     @Test
     void createOrder() {
