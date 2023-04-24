0d57b8322ba19f831624cae8f7e1091fe525ba6f
류원창 <wom05169@gmail.com>
2023-04-24 17:52:58 +0900

    jwt 연습 중..2

---
+++ jpashop/src/main/java/jpabook/jpashop/dto/TokenDto.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+
+    private String token;
+}
---
+++ jpashop/src/main/java/jpabook/jpashop/dto/UserDto.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jpabook.jpashop.entity.User;
+import lombok.*;
+import org.jetbrains.annotations.NotNull;
+
+import javax.validation.constraints.Size;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+   @NotNull
+   @Size(min = 3, max = 50)
+   private String username;
+
+   @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+   @NotNull
+   @Size(min = 3, max = 100)
+   private String password;
+
+   @NotNull
+   @Size(min = 3, max = 50)
+   private String nickname;
+
+   private Set<AuthorityDto> authorityDtoSet;
+
+   public static UserDto from(User user) {
+      if(user == null) return null;
+
+      return UserDto.builder()
+              .username(user.getUsername())
+              .nickname(user.getNickname())
+              .authorityDtoSet(user.getAuthorities().stream()
+                      .map(authority -> AuthorityDto.builder().authorityName(authority.getAuthorityName()).build())
+                      .collect(Collectors.toSet()))
+              .build();
+   }
+}
